b5d669f29eb9e55bcecdddde2315402a12824b53
Christopher Leung <ckleung@cpp.edu>
2020-09-20 17:10:25 -0700

    updated try catch block

--- src/client.java
+++ src/client.java
@@ -1,12 +1,24 @@
 import java.net.*;
-        import java.io.*;
+import java.io.*;
 public class client {
     public static void main(String[] args)throws IOException{
+        Socket socket = null;
         try {
-            Socket socket = new Socket("localhost", 4999);
+            socket = new Socket("localhost", 4999);
         }
-        catch (Exception e){
-            System.out.println("ERROR: Could not establish new connection to server");
+        catch (SecurityException e){
+            System.out.println("CLIENT ERROR: Security manager blocked creation of socket\r\nExiting Program.");
+            System.exit(-1);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("CLIENT ERROR: Could not create socket since port is out of range." +
+                               "\r\nAcceptable ports: 0 and 65535 inclusive" +
+                               "\r\nExiting Program.");
+            System.exit(-1);
+        }
+        catch (IOException e){
+            System.out.println("CLIENT ERROR: Could not create socket (IOException Throw)\r\nExiting Program.");
+            System.exit(-1);
         }
 
     }
--- src/server.java
+++ src/server.java
@@ -3,11 +3,32 @@ import java.io.*;
 
 public class server {
     public static void main(String[] args)throws IOException{
-        int timeout = 500;
-        ServerSocket serverSocket = new ServerSocket(4999);
-        Socket socket = serverSocket.accept();
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        try {
+             serverSocket = new ServerSocket(4999);
+             socket = serverSocket.accept();
+        }
+        catch (SecurityException e){
+            System.out.println("SERVER ERROR: Security manager blocked creation of socket\r\nExiting Program.");
+            System.exit(-1);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("SERVER ERROR: Could not create socket since port is out of range." +
+                               "\r\nAcceptable ports: 0 and 65535 inclusive" +
+                               "\r\nExiting Program.");
+            System.exit(-1);
+        }
+        catch (IOException e){
+            System.out.println("SERVER ERROR: Could not create socket (IOException Throw)\r\nExiting Program.");
+            System.exit(-1);
+        }
+
 
         System.out.println("Connection to client has been established!");
+
+        socket.close();
 
     }
 }
