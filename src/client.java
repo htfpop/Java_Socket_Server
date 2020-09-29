b83893257af308901f80183c983b3793c164f757
Christopher Leung <ckleung@cpp.edu>
2020-09-29 15:02:25 -0700

    From MBP

--- src/client.java
+++ src/client.java
@@ -2,9 +2,19 @@ import java.net.*;
 import java.io.*;
 public class client {
     public static void main(String[] args)throws IOException{
+        if(args.length < 2){
+            System.out.println("Error! Missing arguments!\r\nUsage: java client <Server IP> <Input Data File> <Output Data File>");
+            System.out.println("Exiting Now..");
+            System.exit(-1);
+        }
+        String serverIPAddr = args[0];
+        String argFile = args[1];
+        File inFile = new File(argFile);
+
+
         Socket socket = null;
         try {
-            socket = new Socket("localhost", 4999);
+            socket = new Socket(serverIPAddr, 4999);
         }
         catch (SecurityException e){
             System.out.println("CLIENT ERROR: Security manager blocked creation of socket\r\nExiting Program.");
@@ -21,5 +31,28 @@ public class client {
             System.exit(-1);
         }
 
+        OutputStream outStream = socket.getOutputStream();
+        DataOutputStream dOut = new DataOutputStream(outStream);
+
+        FileInputStream FIS = new FileInputStream(inFile);
+
+
+        long fileSize = inFile.length();
+        byte[] fileData = new byte[(int)fileSize];
+        fileData = FIS.readAllBytes();
+
+        dOut.writeInt((int)fileSize);
+        dOut.flush();
+
+        dOut.write(fileData);
+        dOut.flush();
+
+
+        //dOut.writeUTF("Hello this is the client!");
+        //dOut.flush();
+        dOut.close();
+
+        System.out.println("End of session! Closing socket");
+        socket.close();
     }
 }
